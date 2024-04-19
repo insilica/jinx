cb16431ead5146ac3541413bec1d9adb544eec4e
John Shaffer <john@insilica.co>
2024-04-19 10:22:19 -0500

    Convert to nix flake devShell

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "jinx";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      with import nixpkgs { inherit system; }; {
+        devShells.default = mkShell {
+          buildInputs = [
+            clojure
+            jdk
+            nodejs
+            rlwrap # Used by clj
+          ];
+        };
+      });
+}
